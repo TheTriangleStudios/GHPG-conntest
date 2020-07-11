aef259138e31ac5a726d9eeb66762c88f56f57d1
TheTriangleStudios <68070792+TheTriangleStudios@users.noreply.github.com>
2020-07-11 11:46:46 +0200

    Update styler.js

--- styler.js
+++ styler.js
@@ -1,4 +1,4 @@
 var style = document.createElement("link");
 style.type = "stylesheet";
-style.href = "https://thetrianglestudios.github.io/GHPG-conntest/styler.js";
+style.href = "https://thetrianglestudios.github.io/GHPG-conntest/style.css";
 document.head.appendChild(style);
